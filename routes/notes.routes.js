7f2727cf22d4937e3da44ecab783dacaf00705d5
mk3346 <mk3346@hotmail.de>
2023-06-12 10:50:31 +0200

    added note create route to note.routes

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middleware/isLoggedIn");
 
+const Note = require('../models/Note.model');
+
 // GET /notes/list
 router.get("/notes/list", isLoggedIn, (req, res, next) => {
   const userDetails = req.session.currentUser;
@@ -9,5 +11,56 @@ router.get("/notes/list", isLoggedIn, (req, res, next) => {
     res.render("notes/note-list", { userDetails });
   }
 });
+
+// CREATE new note / display form
+router.get("/notes/create", isLoggedIn, (req, res, next) => {
+  res.render("notes/note-create");
+});
+
+// CREATE new note / process form
+router.post("/notes/create", isLoggedIn, (req, res, next) => {
+  const {
+    title,
+    description,
+    checklist,
+    label,
+    backgroundColor,
+    image
+  } = req.body;
+
+  const userId = req.session.currentUser._id;
+
+  const newNote = {
+    title,
+    description,
+    user: userId
+  };
+
+  if (checklist) {
+    newNote.checklist = checklist;
+  }
+
+  if (label) {
+    newNote.label = label;
+  }
+
+  if (backgroundColor) {
+    newNote.backgroundColor = backgroundColor;
+  }
+
+  if (image) {
+    newNote.image = image;
+  }
+
+  Note.create(newNote)
+    .then((newNote) => {
+      res.redirect("/notes/list");
+    })
+    .catch((e) => {
+      console.log("Error creating new note", e);
+      next(e);
+    });
+});
+
 
 module.exports = router;
